7b75dad7c104392e23d01e198739f4f512e8db76
Kenneth Onu <kennetho.onu@gmail.com>
2022-12-28 19:48:24 +0800

    updated client-side

--- client-side-code/js/main.js
+++ client-side-code/js/main.js
@@ -3,7 +3,7 @@ document.querySelector('button').addEventListener('click', getArtistInfo)
 async function getArtistInfo(){
     const artistName = document.querySelector('input').value
     try{
-        const res = await fetch(`https://cerulean-foal-kit.cyclic.app/api/artists/${artistName}`)
+        const res = await fetch(`https://cerulean-foal-kit.cyclic.app/api/${artistName}`)
         const data = await res.json()
         console.log(data)
         document.querySelector('.birthName').innerText = data.birthName
